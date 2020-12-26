d85956b409b360b83214da7d4acd695dfd566a62
el malki nassim <nassim_big15@live.fr>
2020-12-26 23:59:37 +0000

    first init

---
+++ src/main/java/nassim/youssef/App.java
@@ -0,0 +1,13 @@
+package nassim.youssef;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
